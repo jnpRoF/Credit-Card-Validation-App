4b80e0b9666a362a8ae71cf7c9d3484c46056d88
Johnson <ogbonnayajprof2015@gmail.com>
2023-04-02 22:03:51 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,252 @@
+const allInputs = [...document.querySelectorAll(".inp")];
+const cardDisplay = document.querySelector(".card_display");
+const cardHolderNameInput = document.querySelector(".inp2");
+const cvvInput = document.querySelector(".cvv_inp");
+const cHolderName = document.querySelector(".cholder_name");
+const cvvDisplay = document.querySelector(".white_strip");
+const selectMonth = document.querySelector(".select1");
+const selectYear = document.querySelector(".select2");
+const cHolderExpire = document.querySelector(".cholder_exp");
+const validBtn = document.querySelector(".valid_btn");
+const cardType = document.querySelector(".card_type");
+const cardType2 = document.querySelector(".card_type2");
+let cardNumber = [];
+const state = {
+  firstInput: null,
+  secondInput: null,
+  thirdInput: null,
+  fourthInput: null,
+  fifthInput: null,
+  CARDNUMBER: [],
+  CARDHOLDERNAME: "",
+  CVV: "",
+  expMonth: "",
+  expYear: "",
+  isValid: null,
+};
+const focusNext = () => {
+  const currInput = document.activeElement;
+  const currInputIndex = allInputs.indexOf(currInput);
+  const nextInputIndex = (currInputIndex + 1) % allInputs.length;
+  const input = allInputs[nextInputIndex];
+  input.focus();
+};
+const focusBack = () => {
+  const currInput = document.activeElement;
+  const currInputIndex = allInputs.indexOf(currInput);
+  currInputIndex === 0
+    ? (prevInputIndex = allInputs.length - 1)
+    : (prevInputIndex = (currInputIndex - 1) % allInputs.length);
+
+  const input = allInputs[prevInputIndex];
+  input.focus();
+};
+const setFirstInputNumber = (num) => {
+  state.firstInput = num;
+  state.CARDNUMBER = [
+    state.firstInput,
+    state.secondInput,
+    state.thirdInput,
+    state.fourthInput,
+    state.fifthInput,
+  ];
+};
+const setSecondInputNumber = (num) => {
+  state.secondInput = num;
+  state.CARDNUMBER = [
+    state.firstInput,
+    state.secondInput,
+    state.thirdInput,
+    state.fourthInput,
+    state.fifthInput,
+  ];
+};
+const setThirdInputNumber = (num) => {
+  state.thirdInput = num;
+  state.CARDNUMBER = [
+    state.firstInput,
+    state.secondInput,
+    state.thirdInput,
+    state.fourthInput,
+    state.fifthInput,
+  ];
+};
+const setFourthInputNumber = (num) => {
+  state.fourthInput = num;
+  state.CARDNUMBER = [
+    state.firstInput,
+    state.secondInput,
+    state.thirdInput,
+    state.fourthInput,
+    state.fifthInput,
+  ];
+};
+const setFifthInputNumber = (num) => {
+  state.fifthInput = num;
+  state.CARDNUMBER = [
+    state.firstInput,
+    state.secondInput,
+    state.thirdInput,
+    state.fourthInput,
+    state.fifthInput,
+  ];
+};
+
+const displayCardNumber = () => {
+  cardDisplay.innerHTML = state.CARDNUMBER.join(" ");
+};
+
+const handleValidity = ($valid) => {
+  $valid === true
+    ? (cardDisplay.innerHTML = "VALID")
+    : (cardDisplay.innerHTML = "NOT VALID");
+};
+allInputs.forEach((eachInput) => {
+  eachInput.addEventListener("input", (e) => {
+    if (e.target.value !== "") {
+      let num = e.target.value;
+      //allow only numbers
+      if (num % num === 0 || num == 0) {
+        //0 % 0 !== 0
+        if (document.activeElement) {
+          const currInputIndex = allInputs.indexOf(document.activeElement);
+          const inputNumber = (setInputNumber, i) => {
+            if (currInputIndex === i) {
+              if (i == 0) {
+                if (num.charAt(0) == 5) {
+                  cardType.src = "./Images/mastercard.png";
+                  cardType2.src = "./Images/verve.png";
+                  cardType.style.display = "block";
+                  cardType2.style.display = "block";
+                } else if (num.charAt(0) == 4) {
+                  cardType.style.display = "none";
+                  cardType2.style.display = "block";
+                  cardType2.src = "./Images/visa.png";
+                } else {
+                  cardType.style.display = "none";
+                  cardType2.style.display = "none";
+                }
+              }
+              setInputNumber(num);
+              displayCardNumber();
+            }
+          };
+
+          inputNumber(setFirstInputNumber, 0);
+          inputNumber(setSecondInputNumber, 1);
+          inputNumber(setThirdInputNumber, 2);
+          inputNumber(setFourthInputNumber, 3);
+          inputNumber(setFifthInputNumber, 4);
+        }
+        if (eachInput.value.length >= eachInput.maxLength) {
+          focusNext();
+        }
+
+        if (allInputs[allInputs.length - 1].maxLength) {
+        }
+      }
+    } else {
+      delComplete(setFirstInputNumber, 0);
+      delComplete(setSecondInputNumber, 1);
+      delComplete(setThirdInputNumber, 2);
+      delComplete(setFourthInputNumber, 3);
+      delComplete(setFifthInputNumber, 4);
+      focusBack();
+    }
+  });
+});
+
+const delComplete = (setInputNumber, i) => {
+  const currInputIndex = allInputs.indexOf(document.activeElement);
+  if (currInputIndex === i) {
+    setInputNumber("");
+    displayCardNumber();
+  }
+};
+const displayCardHolderDetails = (cHolderDetailsCont, CARDHOLDERDETAILS) => {
+  cHolderDetailsCont.innerHTML = CARDHOLDERDETAILS;
+};
+cardHolderNameInput.addEventListener("input", () => {
+  state.CARDHOLDERNAME = cardHolderNameInput.value;
+  displayCardHolderDetails(cHolderName, state.CARDHOLDERNAME);
+});
+cvvInput.addEventListener("input", () => {
+  state.CVV = cvvInput.value;
+  displayCardHolderDetails(cvvDisplay, state.CVV);
+});
+
+const cardFront = document.querySelector(".card_front");
+const cardBack = document.querySelector(".card_back_cont");
+
+cvvInput.addEventListener("focus", () => {
+  cardFront.classList.add("front-rotate");
+  cardBack.classList.add("back-rotate");
+});
+
+cvvInput.addEventListener("input", () => {
+  cardFront.classList.add("front-rotate");
+  cardBack.classList.add("back-rotate"); //extract to a function //to enable access to the cvv display on second thought
+  if (cvvInput.value.length === cvvInput.maxLength) {
+    setTimeout(() => {
+      cardFront.classList.remove("front-rotate");
+      cardBack.classList.remove("back-rotate");
+    }, 2000);
+  }
+});
+
+const renderExpiryDate = () => {
+  cHolderExpire.innerHTML = `${state.expMonth}/${state.expYear}`;
+};
+
+selectMonth.addEventListener("input", () => {
+  if (!selectMonth.value) {
+    state.expMonth = "";
+    renderExpiryDate();
+  } else {
+    const expMonth =
+      `${+selectMonth.value + 1}`.length === 1 //use date format "01"
+        ? //"0" + (+selectMonth.value + 1)
+          `0${+selectMonth.value + 1}`
+        : `${+selectMonth.value + 1}`;
+
+    state.expMonth = expMonth;
+
+    renderExpiryDate();
+  }
+});
+selectYear.addEventListener("input", () => {
+  const expYear = selectYear.value.slice(2, 4);
+  state.expYear = expYear;
+  renderExpiryDate();
+});
+
+const ccNumber = (digits, weight = 1, productSum = 0) => {
+  const digitsArr = `${digits}`.split("");
+  if (!digitsArr.length) return productSum % 10 === 0;
+  const lastDigit = digitsArr[digitsArr.length - 1];
+
+  const product = +lastDigit * weight;
+  const getProductSum =
+    `${product}`.length === 2
+      ? +`${product}`.charAt(0) + +`${product}`.charAt(1)
+      : product;
+  productSum += getProductSum;
+  digitsArr.pop();
+  const arr = +digitsArr.join("");
+  return ccNumber(
+    arr === 0 ? "" : arr,
+    (weight = weight === 1 ? 2 : 1),
+    productSum
+  );
+};
+
+validBtn.addEventListener("click", () => {
+  if (!state.CARDNUMBER.length) {
+    alert("Please input a number.");
+  } else {
+    const isValid = ccNumber(+state.CARDNUMBER.join(""));
+    state.isValid = isValid;
+    handleValidity(state.isValid);
+    // console.log(isValid);
+  }
+});
